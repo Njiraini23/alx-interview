89ddf5e868a78a1edfa9fb3424112bfee929abed
Njiraini23 <njiraini88@gmail.com>
2023-09-05 16:38:43 +0000

    0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""determines if dataset contains UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """
+    a method that determines if a given data set represents
+    a valid UTF-8 encoding. returns True if data is a valid UTF-8
+    else returns False. Character be 1 to 4 bytes long
+    """
+    n_bytes = 0
+
+    for num in data:
+        """check the data through the integers in the list"""
+
+        if n_bytes == 0:
+            id byte >> 7 == 0b0:
+                continue
+            elif byte >> 5 == 0b110:
+                n_bytes = 1
+            elif byte >> 4 == ob1110:
+                n_bytes = 2
+            elif byte >> 3 == 0b11110:
+                n_bytes = 3
+            else:
+                return False
+        else:
+            if byte >> 6 == 0b10:
+                n_bytes -= 1
+            else:
+                return False
+
+    return n_bytes == 0
